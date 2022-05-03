201a66fea4430f66e2400cb586a0bf640798f652
saschafuchs <sascha.fuchs@webpros.com>
2022-05-03 10:44:47 +0200

    Build test 7

--- __tests__/client.test.ts
+++
@@ -1,9 +0,0 @@
-import Client from '../src/Client'
-
-describe('test client', () => {
-
-  it('check the client now', () => {
-    const client = new Client()
-   expect(client).toBeDefined()
-  })
-})
